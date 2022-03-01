55768e48518393f04eee37acb0166e7712871141
ssk <developer.kss@gmail.com>
2022-03-01 15:08:32 +0900

    firstWeek ok

--- test/calendar_date_test.dart
+++ test/calendar_date_test.dart
@@ -32,30 +32,39 @@ void main() {
     assert(ListEquality().equals(expected, actual),
         'expected: [$expected] but actual: [$actual]');
   });
+
+  test('first week of 2022-01', () async {
+    final repository = CalendarRepository();
+
+    final expected = ['2021-12-30', '2021-12-31', '2022-01-01'];
+
+    final firstWeek = repository.getCalendarWeeks(year: 2022, month: 1).first;
+    final actual = firstWeek
+        .sublist(4)
+        .map((e) => DateFormat("yyyy-MM-dd").format(e))
+        .toList();
+
+    assert(ListEquality().equals(expected, actual),
+        'expected: [$expected] but actual: [$actual]');
+  });
 }
 
 class CalendarRepository {
   List<List<DateTime>> getCalendarWeeks(
       {required int year, required int month}) {
+    final beginDay = DateTime(year, month, 1);
+    // monday/1 to sunday/7
+    final beforeDays = beginDay.weekday;
+
+    final firstWeek = List.generate(beforeDays,
+            (index) => beginDay.subtract(Duration(days: beforeDays - index))) +
+        List.generate(
+            DateTime.sunday - beginDay.weekday,
+            (index) => beginDay.add(
+                  Duration(days: index),
+                ));
     return [
-      [
-        DateTime(year, 2, 27),
-        DateTime(year, 2, 28),
-        DateTime(year, 3, 1),
-        DateTime(year, 3, 2),
-        DateTime(year, 3, 3),
-        DateTime(year, 3, 4),
-        DateTime(year, 3, 5),
-      ],
-      [
-        DateTime(year, 3, 27),
-        DateTime(year, 3, 28),
-        DateTime(year, 3, 29),
-        DateTime(year, 3, 30),
-        DateTime(year, 3, 31),
-        DateTime(year, 4, 1),
-        DateTime(year, 4, 2),
-      ],
+      firstWeek,
     ];
   }
 }
