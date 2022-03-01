0667bbd46040d0f0e3a21bfbbcc7e21437dd3d18
ssk <developer.kss@gmail.com>
2022-03-01 15:38:34 +0900

    week 코드 작성 완료

--- test/calendar_date_test.dart
+++ test/calendar_date_test.dart
@@ -21,13 +21,13 @@ void main() {
   test('last week of 2022-03', () async {
     final repository = CalendarRepository();
 
-    final expected = ['2022-03-30', '2022-03-31', '2022-04-01', '2022-04-02'];
+    final expected =
+        '2022-03-27, 2022-03-28, 2022-03-29, 2022-03-30, 2022-03-31, 2022-04-01, 2022-04-02'
+            .split(', ');
 
     final lastWeek = repository.getCalendarWeeks(year: 2022, month: 3).last;
-    final actual = lastWeek
-        .sublist(3)
-        .map((e) => DateFormat("yyyy-MM-dd").format(e))
-        .toList();
+    final actual =
+        lastWeek.map((e) => DateFormat("yyyy-MM-dd").format(e)).toList();
 
     assert(ListEquality().equals(expected, actual),
         'expected: [$expected] but actual: [$actual]');
@@ -48,16 +48,16 @@ void main() {
         'expected: [$expected] but actual: [$actual]');
   });
 
-  test('last week of 2022-02', () async {
+  test('last week of 2022-12', () async {
     final repository = CalendarRepository();
 
-    final expected = ['2022-02-27', '2022-02-28', '2022-03-01', '2022-03-02'];
+    final expected =
+        '2022-12-25, 2022-12-26, 2022-12-27, 2022-12-28, 2022-12-29, 2022-12-30, 2022-12-31'
+            .split(', ');
 
-    final lastWeek = repository.getCalendarWeeks(year: 2022, month: 2).last;
-    final actual = lastWeek
-        .take(4)
-        .map((e) => DateFormat("yyyy-MM-dd").format(e))
-        .toList();
+    final lastWeek = repository.getCalendarWeeks(year: 2022, month: 12).last;
+    final actual =
+        lastWeek.map((e) => DateFormat("yyyy-MM-dd").format(e)).toList();
 
     assert(ListEquality().equals(expected, actual),
         'expected: [$expected] but actual: [$actual]');
@@ -67,40 +67,37 @@ void main() {
 class CalendarRepository {
   List<List<DateTime>> getCalendarWeeks(
       {required int year, required int month}) {
-    List<DateTime> firstWeek = getFirstWeek(year, month);
-    List<DateTime> lastWeek = getLastWeek(year, month);
-    return [
-      firstWeek,
-      lastWeek,
-    ];
+    final firstDateOfMonth = DateTime(year, month, 1);
+    final beginDateOfWeek = firstDateOfMonth.subtract(
+      Duration(
+        days: firstDateOfMonth.weekday % DateTime.sunday,
+      ),
+    );
+
+    return List.generate(
+      getCountOfWeek(year, month, beginDateOfWeek),
+      (week) => generateWeek(beginDateOfWeek, week),
+    );
   }
 
-  List<DateTime> getFirstWeek(int year, int month) {
-    final beginDay = DateTime(year, month, 1);
-    // monday/1 to sunday/7
-    final beforeDays = beginDay.weekday;
+  int getCountOfWeek(int year, int month, DateTime beginDateOfWeek) {
+    final firstDateOfNextMonth = DateTime(year, month + 1, 1);
 
-    final week = List.generate(beforeDays,
-            (index) => beginDay.subtract(Duration(days: beforeDays - index))) +
-        List.generate(
-            DateTime.sunday - beginDay.weekday,
-            (index) => beginDay.add(
-                  Duration(days: index),
-                ));
-    return week;
+    final numberOfWeek = ((firstDateOfNextMonth.millisecondsSinceEpoch -
+                        beginDateOfWeek.millisecondsSinceEpoch) /
+                    (24 * 60 * 60 * 1000) +
+                6)
+            .toInt() ~/
+        7;
+    return numberOfWeek;
   }
 
-  List<DateTime> getLastWeek(int year, int month) {
-    final endDay = DateTime(year, month + 1, 0);
-    final beforeDays = endDay.weekday;
-
-    final week = List.generate(beforeDays,
-            (index) => endDay.subtract(Duration(days: beforeDays - index))) +
-        List.generate(
-            DateTime.sunday - endDay.weekday,
-            (index) => endDay.add(
-                  Duration(days: index),
-                ));
-    return week;
+  List<DateTime> generateWeek(DateTime beginDateOfWeek, int week) {
+    return List.generate(
+      7,
+      (day) => beginDateOfWeek.add(
+        Duration(days: week * 7 + day),
+      ),
+    );
   }
 }
