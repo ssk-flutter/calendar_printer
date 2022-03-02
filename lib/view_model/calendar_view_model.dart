cff4f815a570af6e7735204d9d77f7d0484ff0ea
ssk <developer.kss@gmail.com>
2022-03-03 06:29:27 +0900

    landscape print

--- lib/view_model/calendar_view_model.dart
+++ lib/view_model/calendar_view_model.dart
@@ -1,6 +1,7 @@
 import 'package:calendar_printer/view_model/date_controller_view_model.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:pdf/pdf.dart';
 import 'package:printing/printing.dart';
 import 'package:provider/provider.dart';
 import 'package:screenshot/screenshot.dart';
@@ -20,6 +21,7 @@ class CalendarViewModel with ChangeNotifier {
 
     await Printing.layoutPdf(
       usePrinterSettings: true,
+      format: PdfPageFormat.standard.landscape,
       onLayout: (format) => _document
           .createNew(
             bytes: bytes,
@@ -48,14 +50,14 @@ class CalendarViewModel with ChangeNotifier {
 
   Widget buildPreviewWidgetForCapture(BuildContext context) {
     final dateController = context.read<DateControllerViewModel>();
-    final previewWidth = 500.0;
     return Material(
       child: SizedBox(
-        width: previewWidth,
-        height: previewWidth,
+        width: 1000.0,
+        height: 550.0,
         child: ClassicCalendar(
           year: dateController.year,
           month: dateController.month,
+          size: Size(1000, 550),
         ),
       ),
     );
